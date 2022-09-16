df66c66bd3114f0eab9b6bbc4b5ae265cf9303cc
Muchammad Indra <muchammadindra97@gmail.com>
2022-09-16 22:06:00 +0700

    isi fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise( async (resolve) => {
+    let jumlahFilmByEmosi = 0;
+
+    const dataTheaterIXX = await promiseTheaterIXX();
+    dataTheaterIXX.forEach((film) => {
+      if (film.hasil === emosi) jumlahFilmByEmosi++;
+    });
+
+    const dataTheaterVGC = await promiseTheaterVGC();
+    dataTheaterVGC.forEach((film) => {
+      if (film.hasil === emosi) jumlahFilmByEmosi++;
+    });
+
+    resolve(jumlahFilmByEmosi);
+  });
+}
 
 module.exports = {
   promiseOutput,
